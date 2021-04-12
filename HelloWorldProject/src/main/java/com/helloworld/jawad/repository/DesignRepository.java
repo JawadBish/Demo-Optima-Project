113c0d5f6afdb0d570566be0e5769486c5418242
Jawad Bish <Jawad.bish@gmail.com>
2021-04-12 23:39:47 +0300

    REMOVE DESIGNS - not Necessary for my demo

--- HelloWorldProject/src/main/java/com/helloworld/jawad/repository/DesignRepository.java
+++
@@ -1,7 +0,0 @@
-package com.helloworld.jawad.repository;
-
-import com.helloworld.jawad.models.Design;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DesignRepository extends CrudRepository<Design,String> {
-}
